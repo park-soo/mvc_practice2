a1dd16da7e92aba9bf37c188c4c24cdf37d0573a
parkmansoo <parkman.xxx@gmail.com>
2023-09-22 02:20:25 +0900

    리팩토링 및 MVC 프레임워크 만들기 최종

--- src/main/java/org/example/mvc/DispatcherServlet.java
+++ src/main/java/org/example/mvc/DispatcherServlet.java
@@ -1,16 +1,21 @@
 package org.example.mvc;
 
-import org.example.mvc.controller.Controller;
+import org.example.mvc.controller.RequestMethod;
+import org.example.mvc.view.JspViewResolver;
+import org.example.mvc.view.ModelAndView;
+import org.example.mvc.view.View;
+import org.example.mvc.view.ViewResolver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 
 
 @WebServlet("/")
@@ -18,23 +23,60 @@ public class DispatcherServlet extends HttpServlet {
 
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
 
-    private RequestMappingHandlerMapping rmhm;
+    private List<HandlerMapping> handlerMappings;
+
+    private List<HandlerAdapter> handlerAdapters;
+
+    private List<ViewResolver> viewResolvers;
 
     @Override
     public void init() throws ServletException {
-        rmhm = new RequestMappingHandlerMapping();
+        RequestMappingHandlerMapping rmhm = new RequestMappingHandlerMapping();
         rmhm.init();
+
+        AnnotationHandlerMapping ahm = new AnnotationHandlerMapping("org.example");
+        ahm.initialize();
+
+        handlerMappings = List.of(rmhm, ahm);
+        handlerAdapters = List.of(new SimpleControllerHandlerAdapter(), new AnnotationHandlerAdapter());
+        viewResolvers = Collections.singletonList(new JspViewResolver());
     }
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.info("[DispatcherServlet] Service started");
-        try {
-            Controller handler = rmhm.findHandler(request.getRequestURI());
-            String viewName = handler.handleRequest(request, response);
+        log.info("[DispatcherServlet] Service started for URL: {} with Method: {}", request.getRequestURI(), request.getMethod());
 
-            RequestDispatcher requestDispatcher = request.getRequestDispatcher(viewName);
-            requestDispatcher.forward(request, response);
+        String requestURI = request.getRequestURI();
+        RequestMethod requestMethod = RequestMethod.valueOf(request.getMethod());
+
+        try {
+            if ("/favicon.ico".equals(request.getRequestURI())) {
+                return; // Favicon 요청에 대해서는 처리하지 않음 (자동으로 찾아서 에러를 발생시키기 때문에 요청을 무시하는 로직 작성.)
+            }
+            Object handler = handlerMappings.stream()
+                    .filter(hm -> hm.findHandler(new HandlerKey(requestMethod, requestURI)) != null)
+                    .map(hm -> hm.findHandler(new HandlerKey(requestMethod, requestURI)))
+                    .findFirst()
+                    .orElseThrow(() -> new ServletException("No handler for [" + requestMethod + ", " + requestURI + "]"));
+
+            //redirect vs forward
+            //String viewName = handler.handleRequest(request, response);
+            //redirect vs forward 2가지를 사용하기위해 아래의 코드를 리팩토링을 해야됨.
+//            RequestDispatcher requestDispatcher = request.getRequestDispatcher(viewName);
+//            requestDispatcher.forward(request, response);
+
+            HandlerAdapter handlerAdapter = handlerAdapters.stream()
+                    .filter(ha -> ha.supports(handler))
+                    .findFirst()
+                    .orElseThrow(() -> new ServletException("No adapter for handler [" + handler + "]"));
+
+            ModelAndView modelAndView = handlerAdapter.handle(request, response, handler);
+
+            for (ViewResolver viewResolver : viewResolvers) {
+                View view = viewResolver.resolveView(modelAndView.getViewName());
+                view.render(modelAndView.getModel(), request, response);
+            }
 
         } catch (Exception e) {
             log.error("exception occurred: [{}]", e.getMessage(), e);
